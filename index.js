d1f0778985561a83b0686def39734481059e4020
talkasrul <talkasrul@gmail.com>
2018-07-25 17:28:13 +0700

    Promise hel jadi promise sesungguhnya

--- index.js
+++ index.js
@@ -14,39 +14,38 @@ function readFilePromise(filename) {
 	});
 }
 
+// object.then
+
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
 	// your code here... (p.s. readFilePromise function(s) should be around here..)
+	let dataParent = null; // tampung dulu kata mbak icha biar bisa
 	readFilePromise(parentFileName)
-		.then((dataParent) => {
-			// console.log(dataParent)
-			readFilePromise(childrenFileName)
-				.then((dataChild) => {
-
-					for(let i in dataParent) {
-						dataParent[i].children = [];
-						for(let j in dataChild) {
-							if(dataChild[j].family == dataParent[i].last_name) {
-								dataParent[i].children.push(dataChild[j].full_name)
-							}
-							// console.log(dataChild[j])
-						}
-						console.log(dataParent[i])
-					}
-					
-				})
-				.catch((err) => {
-					console.log('file child not found')
-				})
-		})
-		.catch((err) => {
-			console.log("file parent not found")
-		})
+	.then((parents) => {
+		dataParent = parents;
+		let dataChildren = readFilePromise(childrenFileName)
+		return dataChildren		//objectPromise
+	})	
+	.then((dataChildren) => {
+		for(let i in dataParent) {
+			dataParent[i].children = [];
+			for(let j in dataChildren) {
+				if(dataChildren[j].family == dataParent[i].last_name) {
+					dataParent[i].children.push(dataChildren[j].full_name)
+				}
+			}
+			console.log(dataParent);
+		}
+			
+	})
+	.catch((err) => {
+		console.log(`file "${err.path}" ----> not found`);
+	})
 	
 }
 
-matchParentsWithChildrens('./parents.json', './childrens.json');
+// matchParentsWithChildrens('./parents.json', './childrens.json');
 console.log("Notification : Data sedang diproses !");
 
 // for Release 2
-matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
+// matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
 matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
