a70f395c8229ca08e80e96f86aa322668c1e066a
talkasrul <talkasrul@gmail.com>
2018-07-25 16:43:12 +0700

    Handel file eror

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ function matchParentsWithChildrens(parentFileName, childrenFileName) {
 					
 				})
 				.catch((err) => {
-					console.log(err)
+					console.log('file child not found')
 				})
 		})
 		.catch((err) => {
-			console.log(err)
+			console.log("file parent not found")
 		})
 	
 }
@@ -48,5 +48,5 @@ matchParentsWithChildrens('./parents.json', './childrens.json');
 console.log("Notification : Data sedang diproses !");
 
 // for Release 2
-// matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
-// matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
+matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
+matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
